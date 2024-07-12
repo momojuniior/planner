4f507c56f61dc1fc6a5eb5ae12ee86e9309e01b3
momojuniior <momo.ju@hotmail.com>
2024-07-11 22:43:27 -0300

    feat: registerParticipantToEvent method implemented

--- planner/src/main/java/com/rocketseat/planner/participant/ParticipantService.java
+++ planner/src/main/java/com/rocketseat/planner/participant/ParticipantService.java
@@ -1,5 +1,7 @@
 package com.rocketseat.planner.participant;
 
+import com.rocketseat.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,8 +9,14 @@ import java.util.UUID;
 
 @Service
 public class ParticipantService {
-    public void registerParticipantsToEvent(List<String> participantsToInvite, UUID tripId) {
 
+    @Autowired
+    private ParticipantRepository repository;
+
+    public void registerParticipantsToEvent(List<String> participantsToInvite, Trip trip) {
+        List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).toList();
+
+        this.repository.saveAll(participants);
     }
 
     public void triggerConfirmationEmailToParticipants(UUID tripId){};
